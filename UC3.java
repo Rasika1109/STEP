9c9ca0590f5051cf21aeb083ecaa7dee5a013af1
Rasika1109 <laasikafogla@gmail.com>
2026-02-16 09:31:54 +0530

    UC3

---
+++ UC3.java
@@ -0,0 +1,12 @@
+public class UC3
+{
+    public static void main()
+        {
+            System.out.println(String.join(" ","***** ","***** ","***** ","***** "));
+            System.out.println(String.join(" ","*   * ","*   * ","*   * ","***** "));
+            System.out.println(String.join(" ","*   * ","*   * ","***** ","    * "));
+            System.out.println(String.join(" ","*   * ","*   * ","*     ","    * "));
+            System.out.println(String.join(" ","***** ","***** ","*     ","***** "));
+        } 
+
+}
