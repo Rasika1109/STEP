f35584d0b33a6e6bd551fdfface5654a3ff3ff79
Rasika1109 <laasikafogla@gmail.com>
2026-03-10 09:33:50 +0530

    UC8

---
+++ week1/arra.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class arra
+{
+    public static void main()
+    {
+        Scanner sc=new Scanner(System.in);
+        String arr[]=new String[50];
+        int[] arr1={1,2,3,4,5};
+        for(int i=0;i<5;i++)
+            System.out.print(arr[i]+"\n");
+        sc.close();
+    }
+}
