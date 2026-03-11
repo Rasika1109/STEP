a7fcb30bdd2175175484ec596ceca07525a120b8
Rasika1109 <laasikafogla@gmail.com>
2026-03-11 09:41:23 +0530

    String variable deletion

--- arra.java
+++ arra.java
@@ -4,10 +4,9 @@ class arra
     public static void main()
     {
         Scanner sc=new Scanner(System.in);
-        String arr[]=new String[50];
         int[] arr1={1,2,3,4,5};
         for(int i=0;i<5;i++)
-            System.out.print(arr[i]+"\n");
+            System.out.print(arr1[i]+"\n");
         sc.close();
     }
 }
